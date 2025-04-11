16236f5d00eaaa4eb8b074995920f7a0e0d9de51
yanis slimani <slimaniyanis141@gmail.com>
2025-04-11 20:46:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex flex-col items-center md:items-start">
    
    with the text content:
    
        📍 Pontoise, France🗣️ Français (natif), Anglais (B2)
    
    Relevant files:
    
    And change it as follows:
    
    text a gauche

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
   return (
     <footer className="py-8">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-4">
-        <div className="flex flex-col items-center md:items-start">
+        <div className="flex flex-col items-start">
           <p className="text-left">📍 Pontoise, France</p>
           <p className="text-left">🗣️ Français (natif), Anglais (B2)</p>
         </div>
-        <div className="flex flex-col items-center md:items-start">
+        <div className="flex flex-col items-start">
           <p className="text-left">📞 0698514119</p>
           <p className="text-left">📧 slimaniyanis143@gmail.com</p>
         </div>
@@ -17,3 +17,4 @@ const Footer = () => {
 };
 
 export default Footer;
+
